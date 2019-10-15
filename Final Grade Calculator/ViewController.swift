3b349edd98e9d64efc864bbe6e518fa09fe97eaf
jakeberry485 <jakeberry1557@gmail.com>
2019-10-15 11:22:54 -0400

    Brendan

--- Final Grade Calculator/ViewController.swift
+++ Final Grade Calculator/ViewController.swift
@@ -8,28 +8,48 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController
+    
 {
+    
+    
+    
+    @IBOutlet weak var TextField3: UITextField!
+    
     @IBOutlet weak var TextField1: UITextField!
     
     @IBOutlet weak var TextField2: UITextField!
+    
+    @IBOutlet weak var Label: UILabel!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+
         
     }
 
+    
+   
     @IBAction func Button(_ sender: UIButton)
     {
+       
+        let num1 = Double(TextField1.text ?? "0") ?? 0
+         
+        let num2 = Double(TextField2.text ?? "0") ?? 0
         
+        let num3 = Double(TextField3.text ?? "0") ?? 0
         
-        
+        let result = (num2 - num1 * (1 - num3)) / num3
+            
+        Label.text = String(result)
         
     }
+    
     
     
     
